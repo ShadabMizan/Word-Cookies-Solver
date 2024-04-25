190fb5f3b183ef75d1b99bba8fb12b6ce6362e2c
Shadab Mizan <shadabsmizan@gmail.com>
2024-04-25 12:11:27 -0700

    Changed templates to all be oriented perpendicular. Added functionality to test all templates through a rotation to overcome the random rotation of letters.

--- word-cookies.py
+++ word-cookies.py
@@ -3,41 +3,52 @@ import time
 import cv2
 from PIL import ImageGrab
 
-# Borders of the entire playable screen are:
+# Borders of the entire playable screen are: (When docked to the left half of the window)
 # (310, 64), (1128, 1514)
 
-# Cookie area is (310, 810) to (1128, 1514)
+# Pan area is (310, 810) to (1128, 1514)
 x1 = 310
 y1 = 64
 x2 = 1128
 y2 = 1514
 
-cookiePanScreenshot = ImageGrab.grab(bbox=(x1, y1, x2, y2))
-cookiePanScreenshot.save('Assets/cookie-pan.png')
+def main():
+    # Grab a screenshot of the word-cookies screen.
+    cookiePanScreenshot = ImageGrab.grab(bbox=(x1, y1, x2, y2))
+    cookiePanScreenshot.save('Assets/cookie-pan.png')
 
-cookiePan = cv2.imread('Assets/cookie-pan.png', 0)
+    cookiePan = cv2.imread('Assets/cookie-pan.png', 0)
 
-letters = ['C','G','M','P','U','Y']
-templates = []
-for letter in letters:
-    t = 'Assets/{}.png'.format(letter)
-    templates.append(cv2.imread(t, 0))
+    # Store all the letters that we know in their imread form in a templates array.
+    letters = ['C','G','M','P','U','Y'] # Letters that we have.
+    templates = []
+    for letter in letters:
+        file_path = 'Assets/{}.png'.format(letter)
+        templates.append(cv2.imread(file_path, 0))
+    
 
-threshold = 0.8
-for template in templates:
-    cookiePanCopy = cookiePan.copy()
-    height, width = template.shape
 
-    result = cv2.matchTemplate(cookiePanCopy, template, cv2.TM_CCOEFF_NORMED)
-    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+    findLetters(templates, letters, cookiePan, 0.8)
 
-    location = max_loc
-    top_left = (location[0], location[1])
-    bottom_right = (location[0] + width, location[1] + height)
-    if max_val >= threshold:
-        cv2.rectangle(cookiePanCopy, top_left, bottom_right, 255, 5)
-        cv2.imshow('Match', cookiePanCopy)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-    else:
-        print("No Match")
+
+def findLetters(templates, letters, img, threshold):
+    lettersFound = []
+    for i in range(0,len(templates)):
+        template = templates[i]
+        height, width = template.shape
+
+        # Need to rotate the template because the letters can spawn in any orientation. Then we need to test all these orientations to see if we get a really good match.
+        # [-16,17] step size 8
+        for angle in range(-16,17,8):
+            rotation_matrix = cv2.getRotationMatrix2D((width / 2, height / 2), angle, 1)
+            rotated_template = cv2.warpAffine(template, rotation_matrix, (width, height))
+            result = cv2.matchTemplate(img, rotated_template, cv2.TM_CCOEFF_NORMED) # Resulting width of this new 2D img array is (W - w + 1, H - h + 1) Where W is the width of the base image adn w is the width of template
+            min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+            if max_val >= threshold:
+                lettersFound.append(letters[i])
+                print(letters[i])
+                break
+
+
+if __name__ == "__main__":
+    main()
