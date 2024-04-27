d8935cd09e29f6dfaf17492786a6349423eda8d2
Shadab Mizan <shadabsmizan@gmail.com>
2024-04-26 17:24:22 -0700

    Fixed detection and added return for letter locations.

--- word-cookies.py
+++ word-cookies.py
@@ -8,10 +8,10 @@ import numpy as np
 # (310, 64), (1128, 1514)
 
 # Pan area is (310, 810) to (1128, 1514)
-x1 = 310
-y1 = 64
-x2 = 1128
-y2 = 1514
+x1 = 0
+y1 = 0
+x2 = 1430
+y2 = 1519
 
 def main():
     # Grab a screenshot of the word-cookies screen.
@@ -21,7 +21,7 @@ def main():
     cookiePan = cv2.imread('Assets/cookie-pan.png', 0)
 
     # Store all the letters that we know in their imread form in a templates array.
-    letters = ['A','B','C','F','G','M','O','P','T','U','Y'] # Letters that we have.
+    letters = ['A','B','C','F','G','M','N','O','P','S','T','U','Y'] # Letters that we have.
     templates = []
     for letter in letters:
         file_path = 'Assets/{}.png'.format(letter)
@@ -29,40 +29,60 @@ def main():
     
 
 
-    foundLetters, locationsLetters = findLetters(templates, letters, cookiePan, 0.8)
-    print(foundLetters)
-    print(locationsLetters)
+    lettersFound, letterLocs = findLetters(templates, letters, cookiePan, 0.8)
+    print(lettersFound)
+    print(letterLocs)
+
+    for location in letterLocs:
+        x = location[0]
+        y = location[1]
+        pyautogui.mouseDown(x, y)
+        time.sleep(1)
+        pyautogui.mouseUp(x,y)
 
 
 def findLetters(templates, letters, img, threshold):
     lettersFound = []
     lettersLocs = []
     lettersVals = []
-    for check in range(2):
-        for i in range(0,len(templates)):
-            template = templates[i]
-            height, width = template.shape
+    # for check in range(2):
+    for i in range(0,len(templates)): # i represents the index in the templates array, which is in alphabetical order
+        # print("Next Letter... ", letters[i])
+        template = templates[i]
+        height, width = template.shape
 
-            # Need to rotate the template because the letters can spawn in any orientation. Then we need to test all these orientations to see if we get a really good match.
-            # [-16,17] step size 8
-            for angle in range(-16,17,8):
-                rotation_matrix = cv2.getRotationMatrix2D((width / 2, height / 2), angle, 1)
-                rotated_template = cv2.warpAffine(template, rotation_matrix, (width, height))
-                result = cv2.matchTemplate(img, rotated_template, cv2.TM_CCOEFF_NORMED) 
-                min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+        # Need to rotate the template because the letters can spawn in any orientation. Then we need to test all these orientations to see if we get a really good match.
+        # [-16,17] step size 8
+        maximum = 0
+        for angle in range(-16,17,8):
+            rotation_matrix = cv2.getRotationMatrix2D((width / 2, height / 2), angle, 1)
+            rotated_template = cv2.warpAffine(template, rotation_matrix, (width, height))
+            result = cv2.matchTemplate(img, rotated_template, cv2.TM_CCOEFF_NORMED) 
+            min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+            
+            max_loc = (max_loc[0] + width/2, max_loc[1] + height/2)
+
+            if max_val > maximum:
+                maximum = max_val
+            
+            if max_val >= threshold: # Match Found
+                if letters[i] not in lettersFound: # Letter is a new addition, meaning that its max_val data and location are also novel.
+                    lettersFound.append(letters[i])
+                    lettersVals.append(max_val)
+                    lettersLocs.append(max_loc)
+                elif letters[i] in lettersFound: # We already have this letter in our list, though due to a shuffle, it may be in a new location with a new max_val
+                    index = lettersFound.index(letters[i])
+                    lettersVals[index] = max_val
+                    lettersLocs[index] = max_loc
+                    
+            
+            # print("Max: {:.3f}".format(maximum))
                 
-                # Match found, but can be false positives. ex. F and P look similar
-                if max_val >= threshold:
-                    if letters[i] not in lettersFound:
-                        lettersFound.append(letters[i])
-                        lettersLocs.append(max_loc)
-                        lettersVals.append(max_val)
-                        if lettersVals[-1] < max_val:
-                            lettersVals[-1] = max_val
-                        break                                
-        print("Shuffling...")    
-        pyautogui.click(360, 1420)
-        time.sleep(1)
+            
+                                              
+        # print("Shuffling...")    
+        # pyautogui.click(360, 1420)
+        # time.sleep(1)
     
     # Threshold to check if a location of an identified letter is too close (i.e. the same spot) as another letter
     thr = 25
@@ -76,7 +96,7 @@ def findLetters(templates, letters, img, threshold):
                 else:
                     indices.append(i)
     indices.sort()
-    indices = list(set(indices))
+    indices = list(set(indices)) # Remove duplicates
     while len(indices) > 0:
         lettersLocs.pop(indices[-1])
         lettersFound.pop(indices[-1])
